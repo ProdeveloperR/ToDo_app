d46f4c381b819a54aca70956cc2412e5b3ec7120
prodeveloper <dany.ourapps99@gmail.com>
2020-04-05 16:31:04 +1000

    everything working fine

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import * as firebase from 'firebase'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+
+  }
+  componentDidMount() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyCM26spKyrQCzYANBB3Ij09p13NU2SzeJQ",
+      authDomain: "gitnreact.firebaseapp.com",
+      databaseURL: "https://gitnreact.firebaseio.com",
+      projectId: "gitnreact",
+      storageBucket: "gitnreact.appspot.com",
+      messagingSenderId: "694787728663",
+      appId: "1:694787728663:web:ecfd799d751ba9ade50352",
+      measurementId: "G-8MCSM8LQTJ"
+    };
+    // Initialize Firebase
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+      //let firestore = firebase.firestore()
+    }
+  }
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Text>todo App</Text>
+      </SafeAreaView>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
